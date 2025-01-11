e25aa37466a401565e9a3e9844303f6e5862d27e
Noelithub77 <noelmcv7@gmail.com>
2025-01-12 02:06:40 +0530

    update manifest.json to include description, orientation, categories, screenshots, and shortcuts

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,28 @@
-// const CACHE_NAME = "bunkialo-cache-v1";
-// const urlsToCache = [
-//   "/",
-//   "index.html",
-//   "src/main.jsx",
-//   "public/icons/bunkialo logo.svg",
-//   // Add other assets you want to cache
-// ];
+const CACHE_NAME = "bunkialo-cache-v1";
+const urlsToCache = [
+  "/",
+  "index.html",
+  "src/main.jsx",
+  "public/icons/bunkialo logo.svg",
+  "screenshots/android screenshot.jpg",
+  "manifest.json"
+];
 
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-//   );
-//   self.skipWaiting();
-// });
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
 
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(self.clients.claim());
-// });
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
 
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request, { ignoreSearch: true }).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
