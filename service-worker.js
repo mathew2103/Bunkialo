eea828c57c766ea74899b11188c97af58bcf9918
Noelithub77 <noelmcv7@gmail.com>
2025-01-12 02:15:54 +0530

    deactivated sw

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,28 @@
-const CACHE_NAME = "bunkialo-cache-v1";
-const urlsToCache = [
-  "/",
-  "index.html",
-  "src/main.jsx",
-  "./icons/bunkialo logo.svg",
-  "./android screenshot.jpg",
-  "manifest.json"
-];
+// const CACHE_NAME = "bunkialo-cache-v1";
+// const urlsToCache = [
+//   "/",
+//   "index.html",
+//   "src/main.jsx",
+//   "./icons/bunkialo logo.svg",
+//   "./android screenshot.jpg",
+//   "manifest.json"
+// ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+//   );
+//   self.skipWaiting();
+// });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(self.clients.claim());
+// });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request, { ignoreSearch: true }).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request, { ignoreSearch: true }).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
