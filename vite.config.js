7cbfd1b0c32e70a2d607c6e95838934656ee77c1
Noelithub77 <noelmcv7@gmail.com>
2025-01-12 10:22:34 +0530

    comment out service worker registration and caching logic in index.html and service-worker.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['src/assets/bunkialo_logo.svg'],
+      includeAssets: ['./icons/bunkialo logo.svg'],
       manifest: {
         name: 'Bunkialo',
         short_name: 'Bunkialo',
@@ -18,42 +18,42 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '72x72',
             type: 'image/svg+xml'
           },
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '96x96',
             type: 'image/svg+xml'
           },
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '128x128',
             type: 'image/svg+xml'
           },
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '144x144',
             type: 'image/svg+xml'
           },
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '152x152',
             type: 'image/svg+xml'
           },
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '192x192',
             type: 'image/svg+xml'
           },
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '384x384',
             type: 'image/svg+xml'
           },
           {
-            src: 'src/assets/bunkialo_logo.svg',
+            src: './icons/bunkialo logo.svg',
             sizes: '512x512',
             type: 'image/svg+xml'
           }
